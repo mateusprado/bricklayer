8d22feee3255ee548b655d4a7587f571ad7c9c93
Rodrigo Sampaio Vaz <rodrigo.vaz@locaweb.com.br>
2010-08-16 03:51:30 -0300

    rest interface complete, and rvm works flawlessly when a project has .rvmrc

--- bricklayer/rest.py
+++ bricklayer/rest.py
@@ -1,75 +1,77 @@
 import os
 import signal
-import logging
 import sys
 
 sys.path.append(os.path.dirname(__file__))
 from projects import Projects
 from builder import Builder
 
-import utils.bottle as bottle
-from utils.bottle import route, request, PasteServer
+import cyclone.web
+import cyclone.escape
+from twisted.internet import defer
+from twisted.python import log
 
-@route('/project', method='POST')
-def project_post():
-    try:
-        Projects().get(request.POST['name'])
-    except Exception, e:
-        project = Projects()
+class Project(cyclone.web.RequestHandler):
+    
+    def post(self):
+        try:
+            Projects().get(self.get_argument('name'))
+        except Exception, e:
+            project = Projects()
+            project.name = self.get_argument('name')[0]
+            project.git_url = self.get_argument('git_url')[0]
+
+            for name, parm in self.request.arguments.iteritems():
+                setattr(project, str(name), parm[0])
+
+            try:
+                project.save()
+                log.msg('Project created: %s', project.name)
+                builder = Builder(project.name)
+                builder.build_project(force=True)            
+                log.msg('Project %s build is done', project.name)
+                self.write(cyclone.escape.json_encode({'status': 'ok'}))
+            except Exception, e:
+                log.err()
+                self.write(cyclone.escape.json_encode({'status': "fail"}))
+        else:
+            self.write(cyclone.escape.json_encode({'status':  "Project already exists"}))
+
+    def put(self, name):
+        project = Projects().get(name)
         builder = Builder(project.name)
-        project.name = request.POST.pop('name')
-        project.git_url = request.POST.pop('git_url')
-
-        for name, parm in request.POST.iteritems():
-            setattr(project, str(name), parm)
+        for name, arg in self.request.arguments.iteritems():
+            setattr(project, name, arg[0])
 
         try:
             project.save()
-            logging.info('Project created: %s', project.name)
-            builder.build_project(force=True)            
-            logging.info('Project %s build is done', project.name)
-            return {'status': 'ok'}
+            builder.build_project(force=True)
+            self.write(cyclone.escape.json_encode({'status': 'ok'}))
         except Exception, e:
-            logging.exception(repr(e))
-            return {'status': "fail"}
-    else:
-        return {'status':  "Project already exists"}
+            log.err(e)
+            self.write(cyclone.escape.json_encode({'status': 'fail'}))
 
-@route('/project/:name', method='PUT')
-def project_put(name):
-    project = Projects().get(name)
-    builder = Builder(project.name)
-    for name, arg in request.POST.iteritems():
-        setattr(project, name, arg)
+    def get(self, name):
+        try:
+            project = Projects().get(name)
+        except Exception, e:
+            self.write(cyclone.escape.json_encode("%s No project found" % e))
+        self.write(cyclone.escape.json_encode({'name': project.name, 
+                'git_url': project.git_url, 
+                'version': project.version,
+                'last_tag': project.last_tag,
+                'last_commit': project.last_commit}))
 
-    try:
-        project.save()
-        builder.build_project(force=True)
-        return {'status': 'ok'}
-    except Exception, e:
-        logging.exception(repr(e))
-        return {'status': 'fail'}
 
-@route('/project/:name', method='GET')
-def project_get(name):
-    try:
-        project = Projects().get(name)
-    except Exception, e:
-        return "%s No project found" % e
-    return {'name': project.name, 
-            'git_url': project.git_url, 
-            'version': project.version,
-            'last_tag': project.last_tag,
-            'last_commit': project.last_commit}
+    def delete(self, name):
+        try:
+            project = Projects().get(name)
+            project.delete()
+        except Exception, e:
+            log.err(e)
 
-@route('/project/:name', method='DELETE')
-def project_delete(name):
-    try:
-        project = Projects().get(name)
-        project.delete()
-    except Exception, e:
-        logging.exception(repr(e))
-
-def run():
-    bottle.run(host='0.0.0.0')
+restApp = cyclone.web.Application([
+    (r'/project/(.*)', Project),
+    (r'/project', Project),
+])
 
